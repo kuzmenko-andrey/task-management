4a3193c1ad8cf5b55569582ba87af5b86ff02386
Kuzmenko Andrey <kuzmenko>
2020-11-03 18:36:02 +0200

    Add database support

---
+++ task-management.data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_management.data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TaskManagerDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ task-management.data/TaskManagerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace task_management.data
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        IConfiguration _configuration;
+
+        public TaskManagerDbContext(IConfiguration configuration) : base()
+        {
+            this._configuration = configuration;
+            DbInitializer.Initialize(this);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
